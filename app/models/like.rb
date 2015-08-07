5e8875e5b803184a7b64923e1c4ad686aece5898
Britney Wright <blw06g@gmail.com>
2015-08-07 15:47:40 -0400

    add model validations, closes #17

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,7 @@
 class Like < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :review
+
+  validates :user_id, :review_id, presence: true
+  validates :user_id, uniqueness: {scope: [:review_id]}
 end
