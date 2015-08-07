2f5450a06a24bb3df681e140a626171b5e42c42b
vic <vicxizhu@gmail.com>
2015-08-07 18:35:42 -0400

    add near places class method to return locations based on name, proximity, and origin latitude and longitude

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,14 @@
 class Location < ActiveRecord::Base
+  acts_as_mappable
   has_many :reviews
-
   validates :name, :lng, :lat, :formatted_address, :place_id, presence: true
+
+  def self.near_places(args = {})
+    name = args.fetch(:name, nil)
+    lat = args.fetch(:lat, nil)
+    lng = args.fetch(:lng, nil)
+    prox = args.fetch(:prox, nil)
+
+    self.within(prox, origin: [lat, lng]).where('LOWER(name) LIKE ?', "%#{name.downcase}%")
+  end
 end
