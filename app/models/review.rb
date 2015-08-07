7476378bb3a21b6185ebb5aa38c44265cac86a52
Britney Wright <blw06g@gmail.com>
2015-08-07 16:15:20 -0400

    fix review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,11 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :location
+  has_many :likes
 
   validates :location_id, :user_id, :overall_rating, presence: true
-  validates_format_of :overall_rating, :cleanliness_rating, :spaciousness_rating, :with => /^[1-5]$/
+  validates :overall_rating, :cleanliness_rating, :spaciousness_rating, numericality: { only_integer: true,
+                                                                                        greater_than_or_equal_to: 1,
+                                                                                        less_than_or_equal_to: 5 
+                                                                                      }
 end
