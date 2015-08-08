a71c80509caca6786566f0108edf2bf58965dd88
Britney Wright <blw06g@gmail.com>
2015-08-08 14:35:34 -0400

    change login success redirect to user_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user.try(:authenticate, params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: "Welcome back #{user.username}!"
+      redirect_to user, notice: "Welcome back #{user.username}!"
     else
       flash[:errors] = ["Invalid Login"]
       redirect_to root_path
