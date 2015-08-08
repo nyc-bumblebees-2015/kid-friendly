4c3c56cd5fc17d4e45d0896184324c19d720ff13
Britney Wright <blw06g@gmail.com>
2015-08-08 11:15:11 -0400

    add location methods for use in views

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,6 +3,7 @@ class Location < ActiveRecord::Base
   has_many :reviews
   validates :name, :lng, :lat, :formatted_address, :place_id, presence: true
 
+
   def self.near_places(args = {})
     name = args.fetch(:name, nil)
     lat = args.fetch(:lat, nil)
@@ -14,5 +15,17 @@ class Location < ActiveRecord::Base
 
   def self.name_places(name)
     self.where('LOWER(name) LIKE ?', "%#{name.downcase}%").to_json
+  end
+
+  def star_rating(attr)
+    "★" * (self.reviews.sum("#{attr}") / self.reviews.count("#{attr} > 0"))
+  end
+
+  def escaped_address
+    self.name.gsub(" ","+") + "+" + self.formatted_address.gsub(" ","+")
+  end
+
+  def amenities?
+    [self.cribs,self.changing_stations,self.high_chairs,self.family_restrooms,self.restrooms,self.nursing_stations,self.water_fountains].any?
   end
 end
