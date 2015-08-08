30bf0638267fcc16ce937a8194db04da3b6c89f4
Britney Wright <blw06g@gmail.com>
2015-08-08 11:37:46 -0400

    create Sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def login
+    user = User.find_by(username: params[:username])
+    if user.try(:authenticate, params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Welcome back #{user.username}!"
+    else
+      flas[:errors] = ["Invalid Login"]
+      redirect_to login_path
+    end
+  end
+
+  def logout
+    session.delete_all
+    redirect_to root_path
+  end
+end
