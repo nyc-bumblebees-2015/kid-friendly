36c7f7229d86245805ea933971529de8eb0e0ba2
Jonathan Chung <hukashi@gmail.com>
2015-08-07 21:00:11 -0400

    add handlebars library, google places api library, work on report_amenities functionality render json objects from api

---
+++ app/assets/javascripts/checkin.js
@@ -0,0 +1,20 @@
+// function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     } else {
+//         x.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+// }
+
+// function showPosition(position) {
+//   $.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + position.coords.latitude + "," + position.coords.longitude + "&radius=500&key=AIzaSyCZUhAwrGykH4KE0SOgd-NYnPymtddT5SA", {crossDomain: true}, function(data){
+//     $("#yo").html(data);
+//   });
+// }
+
+// getLocation();
+
+
+// $(document).ready(function(){
+
+// })
