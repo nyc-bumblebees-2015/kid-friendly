de0ba50fa93a77f2b8d69bb237688a857987abd7
Jonathan Chung <hukashi@gmail.com>
2015-08-08 12:55:20 -0400

    cleaned up code for checkin.js to prepare for form submission

--- app/assets/javascripts/checkin.js
+++ app/assets/javascripts/checkin.js
@@ -3,7 +3,6 @@ var infowindow;
 var lat;
 var lng;
 
-
 function initialize(){
   navigator.geolocation.getCurrentPosition(function(position) {
     lat = position.coords.latitude;
@@ -13,15 +12,11 @@ function initialize(){
 }
 
 function setup() {
-
-
   var pointOfInterest = new google.maps.LatLng(lat, lng);
-
   map = new google.maps.Map(document.getElementById('map-canvas'), {
     center: pointOfInterest,
     zoom: 15
   });
-
   var request = {
     location: pointOfInterest,
     radius: 100,
@@ -38,18 +33,11 @@ function callback(results, status) {
     for (var i = 0; i < results.length; i++) {
       ary.push({name: results[i].name, vicinity: results[i].vicinity, placeID: results[i].place_id});
       // createMarker(results[i]);
-      // $('#locals_list').append("<li>" + results[i].name + " " + results[i].vicinity + "</li>")
     }
-      var context = {locations:ary};//{name: results[i].name, vicinity: results[i].vicinity};
+      var context = {locations:ary};
       var source = $("#google-location-template").html();
       var template = Handlebars.compile(source);
-      // var html = template(context);
       $('#locals_list').append(template(context))
-  console.log(results)
-  console.log(results[1])
-  console.log(results[1].name)
-  console.log(results[1].vicinity)
-
   }
 }
 
@@ -68,26 +56,18 @@ function callback(results, status) {
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
-
 $(document).ready(function(){
   $('body').on('click','.result-row', function(){
     $('.result-row').removeClass('selected-row');
     $(event.target).addClass('selected-row');
-    console.log('shit', event.target);
       var placeID = event.target.dataset.place_id
       var request = {
       placeId: placeID
       };
-
       service = new google.maps.places.PlacesService(map);
       service.getDetails(request, callback);
-
       function callback(place, status) {
         console.log(place);
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-          // createMarker(place);
-        }
       }
   });
 });
-
