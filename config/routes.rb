4f95cf43847cdde7877925337fa42934207ebf89
Britney Wright <blw06g@gmail.com>
2015-08-08 13:14:41 -0400

    set signup route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   resources :users
 
+  get 'signup' => 'users#new'
+
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#logout' 
