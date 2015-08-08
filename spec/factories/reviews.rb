fd2675c846eb2061073d6bc2ab028b6867b6d683
Britney Wright <blw06g@gmail.com>
2015-08-08 19:17:38 -0400

    add review factory for testing

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :review do
-    # location_id
-    # user_id
-    # overall_rating
-    # cleanliness_rating
-    # spaciousness_rating
-    # body
+    association :location 
+    association :user
+    overall_rating 5
+    cleanliness_rating 5
+    spaciousness_rating 5
+    body "This was the best!\nThe greatest place ever!"
   end
 end
