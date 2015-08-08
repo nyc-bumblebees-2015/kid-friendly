41529876ce4954fffaaedd29905972ab6a31900f
Sihong31 <sihongspring2011@gmail.com>
2015-08-08 07:43:35 -0400

    removed and renamed duplicate checkin js files

--- app/assets/javascripts/checkin.js
+++ app/assets/javascripts/checkin.js
@@ -1,20 +1,61 @@
-// function getLocation() {
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(showPosition);
-//     } else {
-//         x.innerHTML = "Geolocation is not supported by this browser.";
-//     }
-// }
 
-// function showPosition(position) {
-//   $.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + position.coords.latitude + "," + position.coords.longitude + "&radius=500&key=AIzaSyCZUhAwrGykH4KE0SOgd-NYnPymtddT5SA", {crossDomain: true}, function(data){
-//     $("#yo").html(data);
+var map;
+var infowindow;
+var lat;
+var lng;
+
+function initialize(){
+  navigator.geolocation.getCurrentPosition(function(position) {
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
+    setup()
+  });
+}
+
+function setup() {
+
+
+  var pointOfInterest = new google.maps.LatLng(lat, lng);
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: pointOfInterest,
+    zoom: 15
+  });
+
+  var request = {
+    location: pointOfInterest,
+    radius: 500,
+    // types: ['store']
+  };
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+}
+
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      // createMarker(results[i]);
+      $('#locals_list').append("<li>" + results[i].name + " " + results[i].vicinity + "</li>")
+    }
+  console.log(results[1])
+  console.log(results[0].name)
+  }
+}
+
+// function createMarker(place) {
+//   var placeLoc = place.geometry.location;
+//   var marker = new google.maps.Marker({
+//     map: map,
+//     position: place.geometry.location
+//   });
+
+//   google.maps.event.addListener(marker, 'click', function() {
+//     infowindow.setContent(place.name);
+//     infowindow.open(map, this);
 //   });
 // }
 
-// getLocation();
+google.maps.event.addDomListener(window, 'load', initialize);
 
 
-// $(document).ready(function(){
-
-// })
