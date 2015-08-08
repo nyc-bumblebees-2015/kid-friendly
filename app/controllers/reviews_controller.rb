40b6dc49237ad2c2a88062e5d5529b0e5bf2e1eb
Britney Wright <blw06g@gmail.com>
2015-08-07 20:56:25 -0400

    create reviews controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+  def index
+  end
+
+  def new
+    @location = Location.find_by(id: params[:location_id])
+    @review = Review.new
+  end
+
+  def create
+    review = Review.new(review_params)
+    review.location = Location.find_by(id: params[:location_id])
+    review.user = User.find_by(id: 1)
+    review.save
+    if review.save
+      redirect_to location_path(review.location), notice: "Review created successfully!"
+    else
+      flash[:errors] = review.errors.full_messages
+      redirect_to location_path(review.location)
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:location_id, :user_id, :overall_rating, :cleanliness_rating, :spaciousness_rating, :body)
+  end
+end
