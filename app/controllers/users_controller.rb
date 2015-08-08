02396b21ea3c9081f9417d2595251536d134b970
Britney Wright <blw06g@gmail.com>
2015-08-08 12:57:48 -0400

    create new user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,51 @@
 class UsersController < ApplicationController
-  def new
-  end
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_authenticated_user, except: [:new, :show] 
 
   def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user, notice: "Thanks for signing up!"
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "Updates saved successfully."
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :edit
+    end    
   end
 
   def destroy
+    if @user.destroy
+      redirect_to root_path, notice: "Account delete succesfully."
+    else
+      redirect_to @user, notice: "Something went wrong. Account still active."
+    end 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :first_name, :last_name, :password)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id])
   end
 end
