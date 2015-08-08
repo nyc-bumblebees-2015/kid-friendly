e7e28b9f2e9a37392496b26df5d771d3fc4d7045
Britney Wright <blw06g@gmail.com>
2015-08-08 11:54:10 -0400

    add logout flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,6 @@ class SessionsController < ApplicationController
 
   def logout
     session.clear
-    redirect_to root_path
+    redirect_to root_path, notice: "Logged out successfully."
   end
 end
