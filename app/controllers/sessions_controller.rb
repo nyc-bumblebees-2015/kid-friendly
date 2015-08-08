3a574c99202e7270ef3901096683efc1a5747b05
Britney Wright <blw06g@gmail.com>
2015-08-08 11:53:31 -0400

    fix typo in login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path, notice: "Welcome back #{user.username}!"
     else
-      flas[:errors] = ["Invalid Login"]
+      flash[:errors] = ["Invalid Login"]
       redirect_to root_path
     end
   end
