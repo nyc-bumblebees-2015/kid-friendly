b3c9fb3e253e6fd5244c2c3c290555d986d1f3ba
Britney Wright <blw06g@gmail.com>
2015-08-08 20:23:56 -0400

    include database cleaner configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,9 +7,16 @@ ActiveRecord::Migration.maintain_test_schema!
 Dir[Rails.root.join("spec/support/*.rb")].each {|f| require f}
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = false
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
   config.include LoginMacros
 end
