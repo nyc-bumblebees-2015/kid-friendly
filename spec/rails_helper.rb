8f54982f850dd9c2720f9866b47c8c10c281eb03
Britney Wright <blw06g@gmail.com>
2015-08-08 20:22:32 -0400

    update to include login_macros

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
-
+Dir[Rails.root.join("spec/support/*.rb")].each {|f| require f}
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
