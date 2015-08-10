b6ec7c4b33f1bc8be19d9022ff9d86a6ed4a328c
britneywright <blw06g@gmail.com>
2015-08-10 09:21:53 -0400

    style liking a review

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   def create
     review = Review.find_by(id: params[:review_id])
     like = Like.new(review: review, user: current_user)
-    if like.save! && request.xhr?
+    if like.save && request.xhr?
       @likes = review.likes.count
       render json: {likes: @likes}.to_json
     else
