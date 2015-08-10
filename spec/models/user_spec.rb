3e90b01215c614e3bbbc13a7f6744bf63f7fe7d5
vic <vicxizhu@gmail.com>
2015-08-10 11:27:13 -0400

    add tests for user create validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,48 @@
 require 'rails_helper'
 
 describe User do
-  it "is valid with a username, first name, last name, email address, and password" do
-    user = User.new(
-      first_name: "John",
-      last_name: "Doe",
-      email: "test@example.com",
-      username: "johndoe",
-      password: "123456")
-    expect(user).to be_valid
+  before(:each) do
+    @user = create(:user)
   end
 
-  it "is invalid without a username"
-  it "is invalid without a first name"
-  it "is invalid without a last name"
-  it "is invalid without an email address"
-  it "is invalid without a properly formatted email address"
-  it "is invalid without a unique username"
-  it "is invalid without a unique email address"
+  it "is valid with a username, first name, last name, email address, and password" do
+    expect(@user).to be_valid
+  end
+
+  it "is invalid without a username" do
+    @user.username = nil
+    expect(@user).to be_invalid
+  end
+
+  it "is invalid without a first name" do
+    @user.first_name = nil
+    expect(@user).to be_invalid
+  end
+
+  it "is invalid without a last name" do
+    @user.last_name = nil
+    expect(@user).to be_invalid
+  end
+
+  it "is invalid without an email address" do
+    @user.email = nil
+    expect(@user).to be_invalid
+  end
+
+  it "is invalid without a properly formatted email address" do
+    @user.email = 'mysteryman.google.com'
+    expect(@user).to be_invalid
+    @user.email = '@'
+    expect(@user).to be_invalid
+  end
+
+  it "is invalid without a unique username" do
+    dup_username_user = build(:dup_username_user)
+    expect(dup_username_user).to be_invalid
+  end
+
+  it "is invalid without a unique email address" do
+    dup_email_user = build(:dup_email_user)
+    expect(dup_email_user).to be_invalid
+  end
 end
