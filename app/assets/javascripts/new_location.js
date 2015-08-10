827accbd762b1b69b8f133d9ee6dc42de6011e66
Sihong31 <sihongspring2011@gmail.com>
2015-08-10 11:48:52 -0400

    test search-data-display div with jQuery

--- app/assets/javascripts/new_location.js
+++ app/assets/javascripts/new_location.js
@@ -10,8 +10,10 @@ function auto() {
       newPlace = autocomplete.getPlace();
         locationData = {name: newPlace.name, place_id: newPlace.place_id, lng: newPlace.geometry.location.K, lat: newPlace.geometry.location.G, formatted_address: newPlace.formatted_address, formatted_phone_number: newPlace.formatted_phone_number}
         console.log(locationData);
+        $('#search-data-display').text(newPlace.formatted_address)
         });
       $("#new_location").on('submit', function(event){
+        event.preventDefault();
         for(var key in locationData){
           $('<input />').attr('type', 'hidden')
           .attr('name', "location[" + key + "]")
