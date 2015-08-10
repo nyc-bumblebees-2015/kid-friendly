a00310f4bab8de2e3b938f31c8afc204aebcb0f0
Sihong31 <sihongspring2011@gmail.com>
2015-08-10 12:07:48 -0400

    fix indentations remove console log from new_location.js

--- app/assets/javascripts/new_location.js
+++ app/assets/javascripts/new_location.js
@@ -6,27 +6,24 @@ function auto() {
 
   var autocomplete = new google.maps.places.Autocomplete(input);
   google.maps.event.addListener(autocomplete, 'place_changed', function () {
-      $('#create-submit-btn').toggle(true);
-      newPlace = autocomplete.getPlace();
-        locationData = {name: newPlace.name, place_id: newPlace.place_id, lng: newPlace.geometry.location.K, lat: newPlace.geometry.location.G, formatted_address: newPlace.formatted_address, formatted_phone_number: newPlace.formatted_phone_number}
-        console.log(locationData);
-        $('#search-data-name').text('Name:' + newPlace.name)
-        $('#search-data-address').text('Address:' + newPlace.formatted_address)
-        $('#search-data-lat').text('Latitude:' + newPlace.geometry.location.G)
-        $('#search-data-lng').text('Longitude:' + newPlace.geometry.location.K)
+    $('#create-submit-btn').toggle(true);
+    newPlace = autocomplete.getPlace();
+    locationData = {name: newPlace.name, place_id: newPlace.place_id, lng: newPlace.geometry.location.K, lat: newPlace.geometry.location.G, formatted_address: newPlace.formatted_address, formatted_phone_number: newPlace.formatted_phone_number}
+    $('#search-data-name').text('Name:' + newPlace.name)
+    $('#search-data-address').text('Address:' + newPlace.formatted_address)
+    $('#search-data-lat').text('Latitude:' + newPlace.geometry.location.G)
+    $('#search-data-lng').text('Longitude:' + newPlace.geometry.location.K)
+    });
 
-        });
-      $("#new_location").on('submit', function(event){
-        event.preventDefault();
-        for(var key in locationData){
-          $('<input />').attr('type', 'hidden')
-          .attr('name', "location[" + key + "]")
-          .attr('value', locationData[key])
-          .prependTo('#new_location');
-        };
-      });
-
+    $("#new_location").on('submit', function(event){
+      event.preventDefault();
+      for(var key in locationData){
+        $('<input />').attr('type', 'hidden')
+        .attr('name', "location[" + key + "]")
+        .attr('value', locationData[key])
+        .prependTo('#new_location');
+      };
+    });
   };
-
 google.maps.event.addDomListener(window, 'load', auto)
 
