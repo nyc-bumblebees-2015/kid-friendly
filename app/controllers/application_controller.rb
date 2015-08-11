71f441a10b38750f7cd79b226f9bbf760526d682
vic <vicxizhu@gmail.com>
2015-08-11 16:56:49 -0400

    made current_user method more efficient by only querying the db for a user if it has to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :current_user?
 
   def current_user
-    User.find_by(id: session[:user_id])
+    @current_user || User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def current_user?(user)
