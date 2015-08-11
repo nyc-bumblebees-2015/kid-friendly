726af61f516816f1446b46ae423abf7b2feb75d5
britneywright <blw06g@gmail.com>
2015-08-11 13:00:07 -0400

    test yelp location integrations

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -31,17 +31,26 @@ FactoryGirl.define do
       place_id "ChIJ60u11Ni3xokRwVg-jNgU9Yk"
       formatted_address "Philadelphia, PA, USA"
       formatted_phone_number " "
-      lat "39.9525839"
-      lng "-75.16522150000003"
+      lat 39.9525839
+      lng -75.16522150000003
     end
 
     factory :international do
       name "Posudion, Magazin"
       formatted_phone_number "8 (495) 620-34-83"
       formatted_address "Red Square, 3, ГУМ, Moskva, Russia, 109012"
-      lat "55.754634"
-      lng "37.62149599999998"
+      lat 55.754634
+      lng 37.62149599999998
       place_id "ChIJBZbmYllKtUYRYI-w1SIN_WU"
+    end
+
+    factory :not_in_yelp do
+      formatted_address "267 3rd Avenue, Brooklyn, NY 11215, United States"
+      formatted_phone_number "(718) 534-0451"
+      lat 40.677791
+      lng -73.98551099999997
+      name "Super 8 Brooklyn / Park Slope Hotel"
+      place_id "ChIJbbhVClVawokRvFU-hqN-lvk"
     end
   end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -21,6 +21,26 @@ RSpec.describe Location do
     expect(create(:international)).to be_valid
   end
 
+  it "gets a yelp id if phone number is found in yelp" do
+    @location.save
+    expect(@location.yelp_id).to be
+  end
+
+  it "does not get a yelp id if phone number is not found in yelp" do
+    notel = create(:not_in_yelp)
+    expect(notel.yelp_id).to be_nil
+  end
+
+  it "gets a yelp url if phone number is found in yelp" do
+    @location.save
+    expect(@location.yelp_url).to be
+  end
+
+  it "does not get a yelp url if phone number is not found in yelp" do
+    notel = create(:not_in_yelp)
+    expect(notel.yelp_url).to be_nil
+  end
+
   it 'is invalid without a name' do
     @location.name = nil
     expect(@location).to be_invalid
@@ -42,10 +62,10 @@ RSpec.describe Location do
     expect(@location).to be_invalid
   end
 
-  it 'has a review' do
+  it 'can have a review' do
     user = create(:user)
-    review = create(:review, user: user, location: @location)
-    expect(@location.reviews.last).to eq(review)
+    expect{
+      review = create(:review, user: user, location: @location)
+    }.to change(@location.reviews, :count).by(1)
   end
-
 end
